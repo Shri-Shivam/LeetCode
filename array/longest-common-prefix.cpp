c7262bf2b6dba6474f793f443473e9dcad3a1dab
SHRI Shivam <25bai71353@cuchd.in>
2026-09-05 23:38:06 +0530

    LC-longest-common-prefix [Runtime: N/A; Memory: N/A]

--- array/longest-common-prefix.cpp
+++ array/longest-common-prefix.cpp
@@ -4,7 +4,7 @@ public:
         string ans="";
         for(int i=0;i<strs[0].size();i++)
         {
-           for(int j=i;j<strs.size();j++)
+           for(int j=1;j<strs.size();j++)
            {
                  if(i>=strs[j].size()||strs[0][i]!=strs[j][i])
                  {
