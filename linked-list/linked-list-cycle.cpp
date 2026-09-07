a1e86325118acf88b72d0b0ae67f063d96e7648b
SHRI Shivam <25bai71353@cuchd.in>
2026-09-07 23:31:16 +0530

    LC-linked-list-cycle [Runtime: N/A; Memory: N/A]

--- linked-list/linked-list-cycle.cpp
+++ linked-list/linked-list-cycle.cpp
@@ -14,7 +14,7 @@ public:
          while(fast!=NULL&&fast->next!=NULL)
          {
             fast=fast->next->next;
-            slow=slow->next->next;
+            slow=slow->next;
             if(fast==slow)
             {
                return true;
